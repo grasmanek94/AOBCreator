1103e10bce51d03900b7028c066845ed336e59bf
Gamer_Z <github@gz0.nl>
2020-03-28 21:48:25 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace AOBCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bufSize = 1024 * 64;
+            Stream inStream = Console.OpenStandardInput(bufSize);
+            Console.SetIn(new StreamReader(inStream, Console.InputEncoding, false, bufSize));
+
+            Console.Write("First input: ");
+            string first = Console.ReadLine().Trim();
+
+            while (true)
+            {
+                Console.Write("Next input: ");
+                string second = Console.ReadLine().Trim();
+
+                string[] fxp = first.Split(' ');
+                string[] sxp = second.Split(' ');
+
+                if (fxp.Length != sxp.Length)
+                {
+                    Console.WriteLine("Bad length: " + fxp.Length.ToString() + " / " + sxp.Length.ToString());
+                    Console.ReadKey();
+                    return;
+                }
+
+                string res = "";
+                int fixups = 0;
+                for (int i = 0; i < fxp.Length; ++i)
+                {
+                    if (fxp[i].ToLower() == sxp[i].ToLower())
+                    {
+                        res += fxp[i] + " ";
+                    }
+                    else
+                    {
+                        ++fixups;
+                        res += "?? ";
+                    }
+                }
+
+                Console.WriteLine("Result: ");
+                Console.WriteLine(res);
+                Console.WriteLine("Bytes changed: " + fixups.ToString() + "/" + fxp.Length.ToString());
+            }
+        }
+    }
+}
